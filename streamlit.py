a461be17aa758f9e45f739a030942109a3b6b469
Akilesh1989 <akilesh.raj@gmail.com>
2021-02-18 01:05:00 +0530

    Minor UI changes

--- streamlit.py
+++ streamlit.py
@@ -3,6 +3,7 @@ import numpy as np
 import os
 import string
 from generating_baby_names import main
+import time
 
 
 st.beta_set_page_config(
@@ -34,43 +35,60 @@ def validate_letter(letter):
 letters = list(string.ascii_lowercase)
 letters.insert(0, 'Any')
 
-input_container, output_container = st.beta_columns(2)
-with input_container:
-    st.subheader("Enter names one after the other. You can also copy and paste. Please make sure you don't enclose them in single or double quotes.")
-    names = st.text_area("Paste or type in names here.")
+st.header("Input")
+
+input_container_1, input_container_2, input_container_3 = st.beta_columns([4, 2, 2])
+with input_container_1:
+    st.subheader("Enter names. ")
+    names = st.text_area("Paste or type in names here. Please make sure you don't enclose them in single or double quotes.",
+                            height=500)
     names = process_names(names)
-    
+
+with input_container_2:
     st.subheader("First letter")
     first_letter = st.selectbox('Select First letter', letters)
     validate_letter(first_letter)
     if first_letter == 'Any':
         first_letter = None
 
+    st.subheader("Minimum name length")
+    minimum_name_length = st.selectbox('Select a minimum length. Lorem ipsum. Lorem ipsum. Lorem ipsum. ', [i for i in range(2, 21)])
+    # minimum_name_length = st.slider('Select a minimum length.', min_value=2, max_value=20)
+
+    st.subheader("Epochs")
+    epochs = st.selectbox("Epochs are the number of times your model trains on the input data. More number of epochs might give you better results, but that is not always the case. It is best to play around with this parameter. To ensure that no one tries to give a million epochs even by accident, I have restricted my epoch count to 4000. This will still take some time to run.",
+                            [10, 100, 200, 400, 800, 1000, 2000, 4000, 6000, 8000, 10000], index=5)    
+
+    button = st.button("Train model and show results.")
+
+with input_container_3:
     st.subheader("Second letter")
     second_letter = st.selectbox('Select Second letter', letters)
     validate_letter(second_letter)
     if second_letter == 'Any':
         second_letter = None
 
-    st.subheader("Minimum name length")
-    # minimum_name_length = st.selectbox('Select a minimum length.', [i for i in range(2, 21)])
-    minimum_name_length = st.slider('Select a minimum length.', min_value=2, max_value=20)
-
     st.subheader("Names in output?")
-    # num_names = st.selectbox("Enter the number of names you want to see in the output. Default value is 10.", [i for i in range(1, 101)])
-    num_names = st.slider("Enter the number of names you want to see in the output. Default value is 10.", min_value=10, max_value=200)
+    num_names = st.selectbox("Enter the number of names you want to see in the output. Default value is 10.", [i for i in range(1, 101)], index=9)
+    # num_names = st.slider("Enter the number of names you want to see in the output. Default value is 10.", min_value=10, max_value=200)
 
-    st.subheader("Epochs")
-    epochs = st.selectbox("Epochs are the number of times your model trains on the input data. More number of epochs might give you better results, but that is not always the case. It is best to play around with this parameter. To ensure that no one tries to give a million epochs even by accident, I have restricted my epoch count to 4000. This will still take some time to run.", [100, 200, 400, 800, 1000, 2000, 4000])    
-    
 
-    button = st.button("Train model and show results.")
+st.header("Output")
+_, output_container_1, _ = st.beta_columns([2, 2,  2])
+with output_container_1:
     if button:
-        st.write("Scroll up and you should see the results to the right.")
-        with output_container:
+        start_time = time.time()
+        with output_container_1:
+            unique_names = []
             with st.spinner("Training model..."):
                     generated_names = main(names, first_letter, second_letter, minimum_name_length, num_names, epochs)
-            for name in generated_names:
+                    end_time = time.time()
+                    time_taken = end_time - start_time
+                    st.markdown(f"**Info:** Model took **{time_taken}** seconds to train.")
+                    for name in generated_names:
+                        if name not in unique_names:
+                            unique_names.append(name)
+            for name in unique_names:
                 st.text(name.replace(".", ""))
         st.balloons()
     
